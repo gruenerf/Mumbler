f9925da6d84ded0834b191155a344fb2a315f9a2
Ferdinand Grüner <ferdinand.gruener@gmx.de>
2015-05-06 11:39:56 +0200

    Added comment

--- app/User.php
+++ app/User.php
@@ -42,6 +42,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 		return $this->hasMany('App\Post');
 	}
 
+	//Todo when stories added uncomment
 	/*public function stories()
 	{
 		return $this->hasMany('App\Story');
