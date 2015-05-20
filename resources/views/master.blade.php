b366a1200b87d18408a9088f4c75b0b17ed13a14
Ferdinand Grüner <ferdinand.gruener@gmx.de>
2015-05-20 03:53:33 +0200

    Undid accidentally done changes

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -36,13 +36,11 @@
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="{{ url('/user/show') }}">Stories</a></li>
 					@if (Auth::guest())
 					    <li><a href="{{ url('/post') }}">Posts</a></li>
 						<li><a href="{{ url('/auth/login') }}">Login</a></li>
 						<li><a href="{{ url('/auth/register') }}">Register</a></li>
 					@else
-						<li><a href="{{ url('/user/show') }}">Profile</a></li>
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
@@ -60,7 +58,7 @@
 
     @yield('header')
 
-    <div class="container" id="content">
+    <div class="content" id="content">
         @yield('content')
     </div>
 
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,50 +1,18 @@
 @extends('master')
 
 @section('content')
-	<div class="row">
+
     @if(count($user))
 
-        <div class="headline col-lg-12"><h1>{{ $user[0]['name'] }} UserName</h1></div>
-        <div class="col-lg-10"><div style="float:right"><button class="btn btn-info dropdown" style="margin: 5px;">Sort By<span class="caret"></span></button><button class="btn btn-info" style="margin: 5px;">Search</button></div></div>
+        <div class="headline">{{ $user[0]['name'] }}</div>
 
-        <div class="user_posts col-lg-5">
-        	<h3>My posts</h3><br>
-    		<p><h4>Lorem</h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    		<p><h4>Ipsum</h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="user_posts">
+
         </div>
 
-        <div class="user_stories col-lg-5">
-        	<h3>My stories</h3><br>
-        	<p><h4>Lorem</h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        	<p><h4>Ipsum</h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        	<p><h4>Dolor</h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="user_stories">
+
         </div>
-        <div class="user_profile col-lg-2"><div style="width:200px; height: 200px; background:teal; border-radius: 100px;"></div><div style="width:200px; height: 200px; background:#999; margin-top:40px; padding:10px; border-radius:3px;"><h4>About me</h4><p><b>Name: </b>Josè</p><p><b>Age: </b>32</p><p><b>Location: </b>FelipeTown</p></div><div style="width:200px; height: 200px; background:#345a5a; margin-top:40px; color:#2fc7c7; word-wrap: break-word; padding: 15px;"><h4>Most Used Hastags</h4><p>#Hastag,#Hastag,#Hastag,#Hastag,#Hastag,#Hastag,#Hastag,#Hastag,#Hastag</p></div></div>
     @endif
-	</div>
+
 @stop
